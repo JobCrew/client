a8195dda61e807a71e61efc31309d5b841c3b898
8woes <seowoo0912@naver.com>
2025-10-13 18:17:05 +0900

    chore: eslint, prettier 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,9 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+import prettierConfing from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier/recommended";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -11,6 +14,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // ESLint-Plugin-Prettier 추가(Prettier 규칙을 ESLint 규칙처럼 적용)
+  prettierPlugin,
+
+  // ESLint-Config-Prettier 추가(Prettier와 충돌하는 모든 규칙 비활성화)
+  prettierConfing,
   {
     ignores: [
       "node_modules/**",
